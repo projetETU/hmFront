1aa5cd26d898d8b84d47efc6f1ac206faf683321
projetETU <men.malala@gmail.com>
2026-07-13 18:01:52 +0300

    miala recent order

--- src/views/Ecommerce.vue
+++ src/views/Ecommerce.vue
@@ -15,9 +15,9 @@
           <statistics-chart />
         </div>
 
-        <div class="col-span-12 sm:col-span-4 xl:col-span-12">
+        <!-- <div class="col-span-12 sm:col-span-4 xl:col-span-12">
           <recent-orders />
-        </div>
+        </div> -->
       </div>
     </div>
   </admin-layout>
