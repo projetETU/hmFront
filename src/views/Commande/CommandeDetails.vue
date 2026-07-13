349215baa51589922467d7b5b8071f120c13c3b7
projetETU <men.malala@gmail.com>
2026-07-13 18:00:05 +0300

    ligne fini

--- src/views/Commande/CommandeDetails.vue
+++ src/views/Commande/CommandeDetails.vue
@@ -619,7 +619,7 @@ const generatePDF = () => {
     doc.setFont(undefined, "normal");
     
     doc.text(ligne1, startX + 90, 72);
-    doc.text(ligne2, startX + 90, 75);
+    doc.text(ligne2, startX + 90, 77);
 
     // =========================
     // TABLEAU
