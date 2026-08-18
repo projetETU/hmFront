40cfcaf368ee4ab89bdfe9e2010683604ece93e0
projetETU <men.malala@gmail.com>
2026-08-18 08:20:05 +0300

    test local

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
-
 const api = axios.create({
   // baseURL: "https://hmflow-production.up.railway.app",
   baseURL: "http://localhost:8080",
 
   withCredentials: true, 
 });
-
 export default api;
